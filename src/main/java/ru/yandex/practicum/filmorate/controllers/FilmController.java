80faf3f027e41a271ee57ba0d39b850eb87eeedf
Евгений Варлахов <e.varlakhov@yandex.ru>
2022-07-24 12:49:00 +0300

    fix:использовать @slf4j.установить константу LocalDate.style: удалить отступы tab.

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -11,12 +10,13 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
     private long idNumber = 1;
     private final Map<Long, Film> films = new HashMap<>();
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private final LocalDate VALIDDATE = LocalDate.of(1895, 12, 28);
 
     @GetMapping
     public Collection<Film> getAllFilms() {
@@ -36,8 +36,8 @@ public class FilmController {
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
         if (!films.containsKey(film.getId())) {
-            log.error("Ошибка при обновлении фильма.Фильма с таким ID не существует.{}", film);
-            throw new RuntimeException("Фильма с таким ID не существует.");
+            log.error("Ошибка при обновлении фильма.Фильма с таким ID не существует.'{}'", film.getId());
+            throw new ValidationException("Фильма с таким ID не существует.");
         }
         films.put(film.getId(), film);
         log.info("Фильм ID-{} обновлен.{}", film.getId(), film);
@@ -49,7 +49,7 @@ public class FilmController {
             log.error("Ошибка при создании фильма: название фильма не указано.'{}'", film.getName());
             throw new ValidationException("Название фильма не может быть пустым.");
         }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+        if (film.getReleaseDate().isBefore(VALIDDATE)) {
             log.error("Ошибка при создании фильма: дата релиза раньше 28.12.1895. '{}'", film.getReleaseDate());
             throw new ValidationException("Дата релиза не может быть раньше 28.12.1895.");
         }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -11,13 +10,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private long idNumber = 1;
     private final Map<Long, User> users = new HashMap<>();
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public Collection<User> getAllFilms() {
